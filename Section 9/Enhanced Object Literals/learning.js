d2dcc1dbdaf8efa5c238356e71493ab891dedc5d
Trung Dao <trungddph27823@fpt.edu.vn>
2023-01-30 13:44:11 +0700

    112. Enhanced Object Literals

---
+++ Section 9/Enhanced Object Literals/learning.js
@@ -0,0 +1,48 @@
+
+const weekday = ['mon','tue','wed','thu','fri','sat','sun']
+// enhanced obj literal we can shortcut some thing in obj like this
+let openingHours = {
+    //enhance obj literals with name of obj  
+    [weekday[3]]: {
+        open: 12,
+        close: 22
+    },
+    [weekday[3]]: {
+        open: 11,
+        close: 23
+    },
+    [weekday[4]]: {
+        open: 0,
+        close: 24
+    },
+} 
+
+let restaurant = {
+    name: 'Classico Italiano',
+    location: 'Van tien dung',
+    categories: ['Italan', 'Pizzaria', 'Oragenic', 'Vegetarian'],
+    starterMenu: ['Forcaccia', 'Bruschetta', 'Gralic bread', 'Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    //es6 enhanced obj literal
+    openingHours,
+    // make function order in the restaurant to get what customer want in starterMenu and mainMenu  
+        // enhanced obj literal with function it still work
+    order (starterIndex, mainIndex){
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    } ,
+    // enhanced obj literal with function it still work
+    orderDelivery({time=1,adress='no adress',mainIndex= 0,starterIndex=1}){
+        //using template string
+        console.log(`Order received! ${this.starterMenu[starterIndex]}
+        and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`)
+    },
+    // enhanced obj literal with function it still work
+
+    orderPizza(mainIngredient, ...ortherIngredient){
+        console.log(mainIngredient)
+        console.log(ortherIngredient)
+    }
+    
+};
+console.log(restaurant.openingHours)
+restaurant.orderPizza('fish','tomato')
--- Section 9/The for-of Loop/learning.js
+++ Section 9/The for-of Loop/learning.js
@@ -1,8 +1,8 @@
 let restaurant = {
-    name : 'Classico Italiano',
+    name: 'Classico Italiano',
     location: 'Van tien dung',
-    categories : ['Italan', 'Pizzaria','Oragenic','Vegetarian'],
-    starterMenu : ['Forcaccia', 'Bruschetta','Gralic bread','Salad'],
+    categories: ['Italan', 'Pizzaria', 'Oragenic', 'Vegetarian'],
+    starterMenu: ['Forcaccia', 'Bruschetta', 'Gralic bread', 'Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
     openingHours: {
         thu: {
@@ -17,35 +17,35 @@ let restaurant = {
             open: 0,
             close: 24
         },
-    } ,
+    },
     // make function order in the restaurant to get what customer want in starterMenu and mainMenu  
-    order : function(starterIndex, mainIndex){
+    order: function (starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-    } ,
+    },
 
-    orderDelivery: function({time=1,adress='no adress',mainIndex= 0,starterIndex=1}){
+    orderDelivery: function ({ time = 1, adress = 'no adress', mainIndex = 0, starterIndex = 1 }) {
         //using template string
         console.log(`Order received! ${this.starterMenu[starterIndex]}
         and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`)
     },
-    orderPizza: function(mainIngredient, ...ortherIngredient){
+    orderPizza: function (mainIngredient, ...ortherIngredient) {
         console.log(mainIngredient)
         console.log(ortherIngredient)
     }
-    
+
 };
-const menu = [...restaurant.mainMenu,...restaurant.starterMenu];
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
 // for of we can use break and continue with for - of
-for(let item of menu) {
+for (let item of menu) {
     console.log(item) // each element
 }
 // for of doesnt have index so if u want that u can do this way
-for(let item2 of menu.entries()) {
+for (let item2 of menu.entries()) {
     console.log(item2) // [index, value]
 }
 
 //using with destructuring
 
-for(let [index,item] of menu.entries()) {
-    console.log(`${index +1}: ${item}`)
+for (let [index, item] of menu.entries()) {
+    console.log(`${index + 1}: ${item}`)
 }
